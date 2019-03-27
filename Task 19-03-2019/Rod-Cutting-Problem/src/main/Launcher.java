becf4f81a5f149dbed09901fcd369c4f48b2bd42
Deepak Yadav <34516539+deepakyadav23@users.noreply.github.com>
2019-03-27 19:37:44 +0530

    fsfsfs

--- Task 19-03-2019/Rod-Cutting-Problem/src/main/Launcher.java
+++ Task 19-03-2019/Rod-Cutting-Problem/src/main/Launcher.java
@@ -1,7 +1,28 @@
 package main;
 
+import java.util.Scanner;
+
 public class Launcher {
+    private static int timesRun = 0;
+    private static void cutter(int n) {
+        if (n == 1) {
+            System.out.println(timesRun);
+            timesRun=0;
+            return;
+        }
+        if (n % 2 == 0) {
+            cutter(n / 2);
+        } else {
+            timesRun++;
+            cutter(n / 2);
+        }
+    }
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            cutter(scanner.nextInt());
+        }
+
     }
 }
